a6cda7f86d3384e606c9a159c99cfa1bc2792cfc
gabrielgrc <gabriel.bsb98@gmail.com>
2023-02-21 17:37:17 -0300

    :sparkles: Adicionada funcionalidade para listar os nomes cadastrados no bd

--- node/index.js
+++ node/index.js
@@ -5,9 +5,18 @@ const db_con = db_funcs.createSqlConnection();
 db_funcs.createTable(db_con);
 
 app.get("/", (req, res) => {
-	res.send('<h1>Full Cycle !</h1>')
-
     db_funcs.insertRandomPerson(db_con);
+    var sql = 'SELECT name from people'
+    query_return = db_con.query(sql, function(err, result){
+        if (err) throw err;
+        console.log("Values selected");
+        console.log(result)
+        let text = "<table border='1'>"
+        for (let obj in result){
+            text += "<tr><td>" + result[obj].name + "</td></tr>"
+        }
+        res.send('<h1>Full Cycle Rocks!</h1>' + `${text}`)
+    }); 
 });
 
 app.listen(3000, () => {
